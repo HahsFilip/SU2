d09ccf02b0aaabf5c66297499178b85ae000de8c
Filip Hahs <filip.hahs@gmail.com>
2023-06-07 21:55:21 +0200

    restarts almost working correctly

--- SU2_PY/checkpoint_tests.py
+++ SU2_PY/checkpoint_tests.py
@@ -50,42 +50,22 @@ def main():
 
     options.partitions  = int( options.partitions )
 
-    sys.stdout.write('\n-------------------------------------------------------------------------\n')
-    sys.stdout.write('|    ___ _   _ ___                                                      |\n')
-    sys.stdout.write('|   / __| | | |_  )   Release 7.5.1 \"Blackbird\"                         |\n')
-    sys.stdout.write('|   \\__ \\ |_| |/ /                                                      |\n')
-    sys.stdout.write('|   |___/\\___//___|   Aerodynamic Shape Optimization Script             |\n')
-    sys.stdout.write('|                                                                       |\n')
-    sys.stdout.write('-------------------------------------------------------------------------\n')
-    sys.stdout.write('| SU2 Project Website: https://su2code.github.io                        |\n')
-    sys.stdout.write('|                                                                       |\n')
-    sys.stdout.write('| The SU2 Project is maintained by the SU2 Foundation                   |\n')
-    sys.stdout.write('| (http://su2foundation.org)                                            |\n')
-    sys.stdout.write('-------------------------------------------------------------------------\n')
-    sys.stdout.write('| Copyright 2012-2023, SU2 Contributors (cf. AUTHORS.md)                |\n')
-    sys.stdout.write('|                                                                       |\n')
-    sys.stdout.write('| SU2 is free software; you can redistribute it and/or                  |\n')
-    sys.stdout.write('| modify it under the terms of the GNU Lesser General Public            |\n')
-    sys.stdout.write('| License as published by the Free Software Foundation; either          |\n')
-    sys.stdout.write('| version 2.1 of the License, or (at your option) any later version.    |\n')
-    sys.stdout.write('|                                                                       |\n')
-    sys.stdout.write('| SU2 is distributed in the hope that it will be useful,                |\n')
-    sys.stdout.write('| but WITHOUT ANY WARRANTY; without even the implied warranty of        |\n')
-    sys.stdout.write('| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |\n')
-    sys.stdout.write('| Lesser General Public License for more details.                       |\n')
-    sys.stdout.write('|                                                                       |\n')
-    sys.stdout.write('| You should have received a copy of the GNU Lesser General Public      |\n')
-    sys.stdout.write('| License along with SU2. If not, see <http://www.gnu.org/licenses/>.   |\n')
-    sys.stdout.write('-------------------------------------------------------------------------\n')
+
 
     checkpoints( options.filename)
 
 #: main()
 def start_from_checkpoint(needed_step, checkpoint_array, conf):
     konfig = copy.deepcopy(conf)
-    restart_point = min(checkpoint_array, key=lambda x:abs(x-needed_step));
+    restart_point = max([i for i in checkpoint_array if needed_step > i])
     time_step = 1
     current_iter = restart_point
+    if restart_point == 0:
+        konfig['RESTART_SOL'] = 'NO'
+        konfig['TIME_ITER'] = time_step+1
+        SU2.run.direct(konfig)
+        konfig['RESTART_SOL'] = 'YES'   
+        current_iter +=1
     for i in range(needed_step-restart_point):
         sys.stdout.write(str(i))
 
@@ -102,6 +82,8 @@ def start_from_checkpoint(needed_step, checkpoint_array, conf):
 
 def checkpoints( filename, partitions  = 0):
     # Config
+    number_of_time_steps = 50
+    check_spacing = 20
     config = SU2.io.Config(filename)
     config.NUMBER_PART = partitions
     sys.stdout.write(str(filename))
@@ -117,23 +99,23 @@ def checkpoints( filename, partitions  = 0):
     SU2.run.direct(config)
     config['RESTART_SOL'] = 'YES'
     current_iter += 1
-    check_pts = range(0,25,10)
+    check_pts = range(0,number_of_time_steps,check_spacing)
 
-    for i in range(25):
+    for i in range(number_of_time_steps):
         sys.stdout.write(str(i))
         
         config['TIME_ITER'] = current_iter + time_step
         config['RESTART_ITER'] = current_iter
         current_iter += time_step
         SU2.run.direct(config)
-        if current_iter-3 not in check_pts and current_iter-2 not in check_pts:
+        if current_iter-3 not in check_pts:# and current_iter-2 not in check_pts:
             if current_iter-3 < 10:
                 os.remove("restart_flow_0000"+str(current_iter-3)+".dat")
             else:
                 os.remove("restart_flow_000"+str(current_iter-3)+".dat")
 
     start_from_checkpoint(13, check_pts, config)
-
+    start_from_checkpoint(67, check_pts, config)
     config['MATH_PROBLEM'] = 'DISCRETE_ADJOINT'
     config['UNST_ADJOINT_ITER'] = current_iter-1
     config['TIME_ITER'] = 1
