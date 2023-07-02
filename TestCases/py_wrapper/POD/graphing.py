e7185d92994a8b229700aea78cbe99a9826b1436
Filip Hahs <filip.hahs@gmail.com>
2023-07-02 21:00:21 +0200

    add some stuff

--- TestCases/py_wrapper/POD/graphing.py
+++ TestCases/py_wrapper/POD/graphing.py
@@ -1,18 +1,37 @@
+import time
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+def animate(i):
+    line  = ax1.tripcolor(x,y,modes[i%10, :,which_field])
+    time.sleep(1)
+    return line,
 x = np.load("x.npy")
 y = np.load("y.npy")
 full_primal = np.load("full.npy")
 un_reduced = np.load("un_reduced.npy")
-values =  np.abs(np.divide(np.abs(full_primal[:,2,400]-un_reduced[:,0,400]),full_primal[:,2,400]))
+modes = np.load("modes.npy")
+which_field = 1
+which_time_step = 350
+values =  np.abs(np.divide(np.abs(full_primal[:,which_field,which_time_step]-un_reduced[:,which_field,which_time_step]),np.mean(full_primal[:,which_field,which_time_step])))
 print(values)
+# print(full_primal[values==np.nan, which_field, which_time_step])
+print(x[values==np.inf])
+print(y[values==np.inf])
 fig1, ax1 = plt.subplots()
 ax1.set_aspect('equal')
-tpc = ax1.tripcolor(x,y,values)
+
+
+tpc = ax1.tripcolor(x,y,modes[0, :,which_field])
 fig1.colorbar(tpc)
 # ax1.set_title('matplotlib.pyplot.tripcolor() Example')
+
+ani = animation.FuncAnimation(fig1, animate, interval=20, blit=True, save_count=50)
 plt.show()
-plt.plot(values)
-plt.show()
+     
+# plt.plot(values)
+# plt.show()
+
+
 # plt.tripcolor(x,y, np.abs(full_primal[:,0,1500]-un_reduced[:,0,1500]))
 # plt.show()
--- TestCases/py_wrapper/POD/tests.py
+++ TestCases/py_wrapper/POD/tests.py
@@ -13,10 +13,14 @@ def animate(i):
 
 n_time_steps = 500
 offset = 1500
-n_of_modes = 25
-n_of_points = 300
-n_of_fields = 3
+n_of_modes = 10
+n_of_points = 4789
+n_of_fields = 11
 ipca = []
+const_fields = [0]*n_of_fields
+is_const = [False]*n_of_fields
+header = ["\"PointID\"","\"x\"","\"y\"","\"Pressure\"","\"Velocity_x\"","\"Velocity_y\"","\"Nu_Tilde\"","\"Pressure_Coefficient\"","\"Density\"","\"Laminar_Viscosity\"","\"Heat_Capacity\"","\"Thermal_Conductivity\"","\"Skin_Friction_Coefficient_x\"","\"Skin_Friction_Coefficient_y\"","\"Heat_Flux\"","\"Y_Plus\"","\"Eddy_Viscosity\""]
+
 for i in range(n_of_fields):
     ipca.append(IncrementalPCA(n_components=n_of_modes, batch_size=n_of_modes, whiten=False))
 
@@ -27,11 +31,21 @@ for j in range(int(n_time_steps/n_of_modes)):
         test = np.loadtxt(file_name, comments="\"", delimiter=",", dtype=float)
         for k in range(n_of_fields):
             fields[i,:,k] = test[0:n_of_points,k+3].reshape(1, -1)
-    print(fields[:,:,k].shape)
-    for k in range(n_of_fields):
-        ipca[k].partial_fit(fields[:,:,k])
+            # print(np.mean(fields[i,:,k]))
+            # print(fields[i,:,k])
+            if np.count_nonzero(fields[i,:,k])> 0.05*n_of_points:
+                
+                const_fields[k] = np.mean(fields[i,:,k])
+                is_const[k] = True
+            # print(is_const)
 
-        print(ipca[k].n_samples_seen_)
+    for k in range(n_of_fields):
+        if is_const[k] == False:
+            
+            ipca[k].partial_fit(fields[:,:,k])
+
+
+print(is_const)
     
 
 reduced_primal = np.empty([n_of_modes, n_time_steps, n_of_fields])
@@ -41,34 +55,53 @@ for i in range(n_time_steps):
     loaded_primal = np.empty([n_of_points, n_of_fields])
     for j in range(n_of_fields):
         loaded_primal[:,j] = test[0:n_of_points,j+3].reshape(1, -1)
-     
-        reduced_primal[:,i,j] =ipca[j].transform(loaded_primal[:,j].reshape(1, -1))
-    print(i)
+        # print(loaded_primal.shape)
+        if is_const[j] == False:
+            reduced_primal[:,i,j] =ipca[j].transform(loaded_primal[:,j].reshape(1, -1))
+    
+    print(i) 
 
 
 un_reduced = np.empty([n_of_points, n_of_fields,n_time_steps])
 full_primal = np.empty([n_of_points, n_of_fields,n_time_steps])
+modes = np.empty([ n_of_modes,n_of_points, n_of_fields])
+title = ""
+for i in range(len(is_const)):
+    
+    title = title + header[i] + " "
 for i in range(n_time_steps):
+    un_reduced_file = open("un_reduced_flow_"+str(i+offset).zfill(5)+".csv", "w+")
+    un_reduced_file.write(title+"\n")
     file_name = "restart_flow_"+str(i+offset).zfill(5)+".csv"
     test = np.loadtxt(file_name, comments="\"", delimiter=",", dtype=float)
     print("Time step: " + str(i))
     for j in range(n_of_fields):
         full_primal[:,j,i] = test[0:n_of_points,j+3].reshape(1, -1)
         print(full_primal.shape)
-        un_reduced[:,j,i] = ipca[j].inverse_transform(reduced_primal[:,i,j])
+        if is_const[j] == False:
+            un_reduced[:,j,i] = ipca[j].inverse_transform(reduced_primal[:,i,j])
+            
+        else:
+            un_reduced[:,j,i] = np.ones(n_of_points)*const_fields[j]
+    np.savetxt(un_reduced_file,np.vstack((np.asarray(range(n_of_points)), test[0:n_of_points, 1].transpose(), test[0:n_of_points,2].transpose(),un_reduced[:,:,i].transpose())).transpose(), delimiter=",")
+
+    un_reduced_file.close()
+for i in range(n_of_fields):
+    if is_const[i] == False:
+        modes[:,:, i] = ipca[i].components_
         # print(str(np.median(np.divide(np.abs(un_reduced[:,j,i]-full_primal[:,j,i]),np.abs(full_primal[:,j,i]))))+" "+str(np.max((np.divide(np.abs(un_reduced[:,j,i]-full_primal[:,j,i]),np.abs(full_primal[:,j,i]))))))
 
 np.save("x", test[0:n_of_points,1])
 np.save("y", test[0:n_of_points:,2]) 
 np.save("full",full_primal)
 np.save("un_reduced", un_reduced)
+np.save("modes", modes)
 
+# fig, ax = plt.subplots()
 
-fig, ax = plt.subplots()
+# line = ax.tripcolor(test[0:n_of_points,1], test[0:n_of_points,2],un_reduced[0:n_of_points,0,0])
+# # plt.colorbar(line, ax=ax.ravel().tolist())
 
-line = ax.tripcolor(test[0:n_of_points,1], test[0:n_of_points,2],un_reduced[0:n_of_points,0,0])
-# plt.colorbar(line, ax=ax.ravel().tolist())
-
-ani = animation.FuncAnimation(fig, animate, interval=20, blit=True, save_count=50)
-plt.show()
+# ani = animation.FuncAnimation(fig, animate, interval=20, blit=True, save_count=50)
+# plt.show()
 
