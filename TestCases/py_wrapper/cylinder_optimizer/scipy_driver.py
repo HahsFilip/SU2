11bbd89ae952d15b5fcb66e92bea9c1b2a6a1e22
Filip Hahs <filip.hahs@gmail.com>
2023-07-27 11:24:05 +0200

    before leaving

--- TestCases/py_wrapper/cylinder_optimizer/scipy_driver.py
+++ TestCases/py_wrapper/cylinder_optimizer/scipy_driver.py
@@ -12,6 +12,7 @@ import SU2
 import subprocess
 import scipy
 def compute_gradients(control_array, configs):
+    global iter
     tmp_dir_name = "tmp_dir_for_grad"
     c_array = "control_array.txt"
     bash_name = "bash_file.sh"
@@ -41,19 +42,23 @@ def compute_gradients(control_array, configs):
     result = subprocess.run("./"+bash_name, shell=True)
     direct_history = np.genfromtxt(configs[0]["CONV_FILENAME"]+".csv", dtype=float, delimiter=',', names=True)
     ad_history = np.genfromtxt(configs[1]["CONV_FILENAME"]+".csv", dtype=float, delimiter=',', names=True)
-    goal_function =np.mean(np.abs(direct_history['CL']-np.sin(np.arange(n_steps)*6.28/20)))
+    goal_function =np.mean(direct_history['ComboObj'])
     os.chdir("..")
     with open("main_history.txt", 'a+') as f:
         f.write(str(goal_function)+"\n")
     gradient = ad_history["Sens_Rot"]
     gradient = gradient[::-1]
     # print(result.stdout.decode('utf-8'))
+    shutil.copytree(tmp_dir_name, tmp_dir_name+"_"+str(iter))
+    iter += 1
     return (goal_function, -1*gradient)
 def main():
+    global iter
+    iter = 2
     direct_config = SU2.io.Config("cylinder.cfg")
     adjoint_config = SU2.io.Config("cylinder_adjoint.cfg")
-    start_vector = np.ones(50)*0
-    compute_gradients(np.zeros(10), (direct_config, adjoint_config))
+    start_vector = np.loadtxt("control_array.txt")
+    # compute_gradients(start_vector, (direct_config, adjoint_config))
     eps = 1000
     # for i in range(100):
     #     (goal, grad) = compute_gradients(start_vector, [direct_config, adjoint_config])
@@ -66,10 +71,10 @@ def main():
     #     start_vector = start_vector+eps*grad
     #     shutil.copytree("tmp_dir_for_grad", "tmp_dir_for_grad_"+str(i))
     opt = {'maxiter': 3}
-    # res  = scipy.optimize.minimize(compute_gradients, start_vector,method='BFGS',args=([direct_config, adjoint_config]), jac=True,  options=opt)
-    # print(res.x)
-    # print(res.message)
-    # print(res.success)
+    res  = scipy.optimize.minimize(compute_gradients, start_vector,method='BFGS',args=([direct_config, adjoint_config]), jac=True,  options=opt)
+    print(res.x)
+    print(res.message)
+    print(res.success)
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
